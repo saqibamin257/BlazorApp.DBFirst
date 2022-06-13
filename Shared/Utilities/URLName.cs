bcafff0895b270f252fd60d401ddfa2bdbb07414
Saqib Amin <saqibamin257@gmail.com>
2022-06-13 10:16:35 +0500

    added separate folders for pages and updated code

---
+++ Shared/Utilities/URLName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.DBFirst.Shared.Utilities
+{
+    public enum URLName
+    {
+        
+    }
+}
