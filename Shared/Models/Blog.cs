dab0c0782caf458ce3a47ab24efa1bb2ec4b2e24
Saqib Amin <saqibamin257@gmail.com>
2022-06-14 15:20:00 +0100

    added validations, and showinng only Home and Manager on nav bar

--- Shared/Models/Blog.cs
+++ Shared/Models/Blog.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorApp.DBFirst.Shared.Models
 {
     public class Blog
     {
+
         public int Blogid { get; set; }
-        public string Title { get; set; }
-        public DateTime CreationDate { get; set; } 
+        [Required]
+        public string Title { get; set; }        
+        public DateTime CreationDate { get; set; }
+        [Required]
         public string PostBody { get; set; } 
         public DateTime PublishedDate { get; set; }
         public bool Status { get; set; }
